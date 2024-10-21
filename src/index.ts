07804cf89de427b3e19300cde78d080bb3f33068
StellanVB <stellan@techfirst.se>
2024-10-21 21:26:20 +0200

    Updated readme file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-export * from "./DirectoryDockClient";
-export * as BaseData from "./types/BaseData";
-export * as BaseEntry from "./types/BaseEntry";
-export * as EntryData from "./types/EntryData";
-export * as FilterableField from "./types/FilterableField";
-export * as GetEntriesResponse from "./types/GetEntriesResponse";
-export * as TransformedEntry from "./types/TransformedEntry";
-export * as Filter from "./types/Filter";
-
+export * from "./DirectoryDockClient";
+export * as BaseData from "./types/BaseData";
+export * as BaseEntry from "./types/BaseEntry";
+export * as EntryData from "./types/EntryData";
+export * as FilterableField from "./types/FilterableField";
+export * as GetEntriesResponse from "./types/GetEntriesResponse";
+export * as TransformedEntry from "./types/TransformedEntry";
+export * as Filter from "./types/Filter";
