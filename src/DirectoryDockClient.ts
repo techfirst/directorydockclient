21db8d2a5ad8fed02a4442019a53fea03c4a8f46
StellanVB <stellan@techfirst.se>
2024-10-21 06:31:51 +0200

    Clean up

--- src/DirectoryDockClient.ts
+++ src/DirectoryDockClient.ts
@@ -1,27 +1,19 @@
 const fetch = require("node-fetch");
-
 const dotenv = require("dotenv");
-
 import { BaseEntry } from "./types/BaseEntry";
-
 import { TransformedEntry } from "./types/TransformedEntry";
-
 import { GetEntriesResponse } from "./types/GetEntriesResponse";
-
 import { FilterableField } from "./types/FilterableField";
-
 import { BaseData } from "./types/BaseData";
 
 dotenv.config();
 
 class DirectoryDockClient {
   private apiKey: string;
-
   private baseURL: string;
 
   constructor(apiKey: string, options: { baseURL?: string } = {}) {
     this.apiKey = apiKey;
-
     this.baseURL =
       options.baseURL ||
       `https://directorydock.blob.core.windows.net/files/${apiKey}`;
@@ -29,83 +21,63 @@ class DirectoryDockClient {
 
   private async fetchFromAPI(
     endpoint: string,
-
     params: Record<string, any> = {}
   ): Promise<any> {
     const url = new URL(`${this.baseURL}/${endpoint}`);
-
     Object.keys(params).forEach((key) =>
       url.searchParams.append(key, params[key])
     );
-
     const response = await fetch(url.toString());
-
     if (!response.ok) {
       if (response.status === 404) {
         throw new Error("Invalid API key");
       }
-
       throw new Error("Network response was not ok");
     }
-
     return await response.json();
   }
 
   private transformEntry(entry: BaseEntry): TransformedEntry {
     const transformedEntry = { ...entry } as TransformedEntry;
-
     Object.keys(entry.data).forEach((key) => {
       Object.defineProperty(transformedEntry, key, {
         get: function () {
           return this.data[key];
         },
-
         enumerable: true,
       });
     });
-
     return transformedEntry;
   }
 
   public async getEntries(
     page: number = 1,
-
     limit: number = 10
   ): Promise<GetEntriesResponse> {
     const params = { page, limit };
-
     const baseData: BaseData = await this.fetchFromAPI(
       "system/base.json",
-
       params
     );
-
     const transformedEntries = baseData.entries.map((entry) =>
       this.transformEntry(entry)
     );
-
     return {
       totalEntries: transformedEntries.length,
-
       entries: transformedEntries,
     };
   }
 
   public async getEntry(slug: string): Promise<TransformedEntry> {
     const params = { slug };
-
     const baseData: BaseData = await this.fetchFromAPI(
       "system/base.json",
-
       params
     );
-
     const entry = baseData.entries.find((e) => e.data.Slug.value === slug);
-
     if (!entry) {
       throw new Error("Entry with the specified slug not found");
     }
-
     return this.transformEntry(entry);
   }
 
@@ -113,23 +85,18 @@ class DirectoryDockClient {
     filters: Record<string, any>
   ): Promise<TransformedEntry[]> {
     const baseData: BaseData = await this.fetchFromAPI("system/base.json");
-
     const filteredEntries = baseData.entries.filter((entry) => {
       return Object.keys(filters).every((filterKey) => {
         const field = entry.data[filterKey];
-
         return field && field.value === filters[filterKey];
       });
     });
-
     return filteredEntries.map((entry) => this.transformEntry(entry));
   }
 
   public async getFilters(): Promise<FilterableField[]> {
     const baseData: BaseData = await this.fetchFromAPI("system/base.json");
-
     const filterableFields: FilterableField[] = [];
-
     for (const entry of baseData.entries) {
       for (const [fieldName, fieldData] of Object.entries(entry.data)) {
         if (
@@ -138,13 +105,11 @@ class DirectoryDockClient {
         ) {
           filterableFields.push({
             name: fieldName,
-
             type: fieldData.type,
           });
         }
       }
     }
-
     return filterableFields;
   }
 }
